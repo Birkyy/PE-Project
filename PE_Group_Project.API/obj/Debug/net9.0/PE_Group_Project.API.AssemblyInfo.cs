088b8d9b0fd47fc8e127ab7ab538835187115f9b
Dexterong9 <dexter.ong6@gmail.com>
2025-07-01 16:18:48 +0800

    -

--- PE_Group_Project.API/obj/Debug/net9.0/PE_Group_Project.API.AssemblyInfo.cs
+++ PE_Group_Project.API/obj/Debug/net9.0/PE_Group_Project.API.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PE_Group_Project.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f540d5890dc85cf35162697449f1fb43612d230")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de5892bc8a8f169420cef61ae969ce57fd985f9")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("PE_Group_Project.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PE_Group_Project.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
