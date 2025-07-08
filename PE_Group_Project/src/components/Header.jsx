7ab730221be23b3acbedf378988a58cf72279bfe
Dexterong9 <dexter.ong6@gmail.com>
2025-07-08 17:38:40 +0800

    Now taskIO button of Admin will redirect to project page

--- PE_Group_Project/src/components/Header.jsx
+++ PE_Group_Project/src/components/Header.jsx
@@ -62,7 +62,7 @@ const Header = ({ onToggleSidebar, darkMode }) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <Link to="/home">
+            <Link to={isUserAdmin() ? "/my-projects" : "/home"}>
               <h1
                 className={`text-2xl font-bold transition-all duration-300 ${
                   darkMode
