43871f011f7534b6ce86c96d5ce3d2a5af96b4cf
Caleb Lau <caleb030221@gmail.com>
2025-07-08 13:17:20 +0800

    -

--- PE_Group_Project.API/obj/Debug/net9.0/PE_Group_Project.API.AssemblyInfo.cs
+++ PE_Group_Project.API/obj/Debug/net9.0/PE_Group_Project.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PE_Group_Project.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8886321c9194630df0b508c5a86a839381f494af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c815bc7b906a84cdb99472ef6ded0b82d049e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PE_Group_Project.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PE_Group_Project.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
