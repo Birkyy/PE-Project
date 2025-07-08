3b3411c9f968b057948cafb6dfc8f102e3f8a548
Dexterong9 <dexter.ong6@gmail.com>
2025-07-08 18:37:14 +0800

    Update User Management View

--- PE_Group_Project/src/pages/UserManagement.jsx
+++
@@ -1,9 +0,0 @@
-function UserManagement() {
-    return (
-        <div className="p-4">
-            <h2 className="text-2x1 font-bold mb-2">User Management Page</h2>
-        </div>
-    );
-}
-
-export default UserManagement;
